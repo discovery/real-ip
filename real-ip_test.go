838d808a8e378f45d3c9fc072fb5065b87d52c21
David Caldwell <david@galvanix.com>
2018-11-10 04:25:08 +0000

    tests: Stop repeating ourselves :-)

--- real-ip_test.go
+++ real-ip_test.go
@@ -21,134 +21,36 @@ func TestUtils_get_remote_addr_net_empty_request(t *T) {
 	assert.NotNil(t, err)
 }
 
-func TestUtils_get_remote_addr_net_invalid_remote_addr_bad_ip_address(t *T) {
-	req := &http.Request{
-		RemoteAddr: "1.2.3.41234:1234",
-		Header: http.Header{
-			"X-Forwarded-For": []string{},
-		},
-	}
-	_, err := remote_addr.GetIP(req)
-	assert.NotNil(t, err)
+func TestStuff(t *T) {
+	DoTest(t, "bad_ip_address",            "", 	  "1.2.3.41234:1234", []string{}, nil)
+	DoTest(t, "missing_port",              "", 	  "1.2.3.4",          []string{}, nil)
+	DoTest(t, "missing_ip_address",        "", 	  ":1234",            []string{}, nil)
+	DoTest(t, "empty_x_forwarded_for",     "1.2.3.4", "1.2.3.4:1234",     []string{""}, nil)
+	DoTest(t, "invalid_x_forwarded_for 1", "1.2.3.4", "1.2.3.4:1234",     []string{"1.2"}, nil)
+	DoTest(t, "invalid_x_forwarded_for 2", "4.4.4.4", "1.2.3.4:1234",     []string{"asdfad, 4.4.4.4"}, nil)
+	DoTest(t, "invalid_x_forwarded_for 3", "1.2.3.4", "1.2.3.4:1234",     []string{"asdfad, 4.4.4.4|"}, nil)
+	DoTest(t, "x_forwarded_for_test 1",    "1.2.3.4", "1.2.3.4:1234",     []string{"127.0.0.1"}, nil)
+	DoTest(t, "x_forwarded_for_test 2",    "4.4.4.4", "1.2.3.4:1234",     []string{"4.4.4.4"}, nil)
+	DoTest(t, "x_forwarded_for_test 3",    "4.4.4.4", "1.2.3.4:1234",     []string{"127.0.0.1, 4.4.4.4"}, nil)
+	DoTest(t, "x_forwarded_for_test 4",    "4.4.4.4", "1.2.3.4:1234",     []string{"127.0.0.1, 4.4.4.4,10.0.0.1"}, nil)
+	DoTest(t, "x_forwarded_for_test 5",    "4.4.4.4", "1.2.3.4:1234",     []string{"8.8.4.4,127.0.0.1, 4.4.4.4,10.0.0.1"}, nil)
 }
 
-func TestUtils_get_remote_addr_net_invalid_remote_addr_missing_port(t *T) {
-	req := &http.Request{
-		RemoteAddr: "1.2.3.4",
-		Header: http.Header{
-			"X-Forwarded-For": []string{},
-		},
-	}
-	_, err := remote_addr.GetIP(req)
-	assert.NotNil(t, err)
-}
-
-func TestUtils_get_remote_addr_net_invalid_remote_addr_missing_ip_address(t *T) {
-	req := &http.Request{
-		RemoteAddr: ":1234",
-		Header: http.Header{
-			"X-Forwarded-For": []string{},
-		},
-	}
-	_, err := remote_addr.GetIP(req)
-	assert.NotNil(t, err)
-}
-
-func TestUtils_GetClientIP_empty_x_forwarded_for(t *T) {
-	req := &http.Request{
-		RemoteAddr: "1.2.3.4:1234",
-		Header: http.Header{
-			"X-Forwarded-For": []string{""},
-		},
-	}
-	res, _ := remote_addr.GetIP(req)
-	assert.Equal(t, net.ParseIP("1.2.3.4"), res)
-}
-
-func TestUtils_GetClientIP_invalid1_x_forwarded_for(t *T) {
-	req := &http.Request{
-		RemoteAddr: "1.2.3.4:1234",
-		Header: http.Header{
-			"X-Forwarded-For": []string{"1.2"},
-		},
-	}
-	res, _ := remote_addr.GetIP(req)
-	assert.Equal(t, net.ParseIP("1.2.3.4"), res)
-}
-
-func TestUtils_GetClientIP_invalid2_x_forwarded_for(t *T) {
-	req := &http.Request{
-		RemoteAddr: "1.2.3.4:1234",
-		Header: http.Header{
-			"X-Forwarded-For": []string{"asdfad, 4.4.4.4"},
-		},
-	}
-	res, _ := remote_addr.GetIP(req)
-	assert.Equal(t, net.ParseIP("4.4.4.4"), res)
-}
-
-func TestUtils_GetClientIP_invalid3_x_forwarded_for(t *T) {
-	req := &http.Request{
-		RemoteAddr: "1.2.3.4:1234",
-		Header: http.Header{
-			"X-Forwarded-For": []string{"asdfad, 4.4.4.4|"},
-		},
-	}
-	res, _ := remote_addr.GetIP(req)
-	assert.Equal(t, net.ParseIP("1.2.3.4"), res)
-}
-
-func TestUtils_GetClientIP_x_forwarded_for_test1(t *T) {
-	req := &http.Request{
-		RemoteAddr: "1.2.3.4:1234",
-		Header: http.Header{
-			"X-Forwarded-For": []string{"127.0.0.1"},
-		},
-	}
-	res, _ := remote_addr.GetIP(req)
-	assert.Equal(t, net.ParseIP("1.2.3.4"), res)
-}
-
-func TestUtils_GetClientIP_x_forwarded_for_test2(t *T) {
-	req := &http.Request{
-		RemoteAddr: "1.2.3.4:1234",
-		Header: http.Header{
-			"X-Forwarded-For": []string{"4.4.4.4"},
-		},
-	}
-	res, _ := remote_addr.GetIP(req)
-	assert.Equal(t, net.ParseIP("4.4.4.4"), res)
-}
-
-func TestUtils_GetClientIP_x_forwarded_for_test3(t *T) {
-	req := &http.Request{
-		RemoteAddr: "1.2.3.4:1234",
-		Header: http.Header{
-			"X-Forwarded-For": []string{"127.0.0.1, 4.4.4.4"},
-		},
-	}
-	res, _ := remote_addr.GetIP(req)
-	assert.Equal(t, net.ParseIP("4.4.4.4"), res)
-}
-
-func TestUtils_GetClientIP_x_forwarded_for_test4(t *T) {
-	req := &http.Request{
-		RemoteAddr: "1.2.3.4:1234",
-		Header: http.Header{
-			"X-Forwarded-For": []string{"127.0.0.1, 4.4.4.4,10.0.0.1"},
-		},
-	}
-	res, _ := remote_addr.GetIP(req)
-	assert.Equal(t, net.ParseIP("4.4.4.4"), res)
-}
-
-func TestUtils_GetClientIP_x_forwarded_for_test5(t *T) {
-	req := &http.Request{
-		RemoteAddr: "1.2.3.4:1234",
-		Header: http.Header{
-			"X-Forwarded-For": []string{"8.8.4.4,127.0.0.1, 4.4.4.4,10.0.0.1"},
-		},
-	}
-	res, _ := remote_addr.GetIP(req)
-	assert.Equal(t, net.ParseIP("4.4.4.4"), res)
+func DoTest(t *T, test_name string, expected string, remoteAddr string, x_forwarded_for []string, extra_headers *http.Header) {
+	t.Run(test_name, func(t *T) {
+		if extra_headers == nil {
+			extra_headers = &http.Header{}
+		}
+		(*extra_headers)["X-Forwarded-For"] = x_forwarded_for
+		req := &http.Request{
+			RemoteAddr: remoteAddr,
+			Header: *extra_headers,
+		}
+		res, err := remote_addr.GetIP(req)
+		if expected == "" {
+			assert.NotNil(t, err)
+		} else {
+			assert.Equal(t, net.ParseIP(expected), res)
+		}
+	})
 }
